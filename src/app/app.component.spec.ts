e84c26ab58d6a15639b716f6705c36c50f00e285
fredyr01 <fredyrios01@gmail.com>
2021-11-03 17:42:48 -0500

    crud empleados con Angular y php

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'crudEmpleados-AngularPhp'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('crudEmpleados-AngularPhp');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crudEmpleados-AngularPhp app is running!');
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
